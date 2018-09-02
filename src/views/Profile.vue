31ddb981900ceadc48bafb06a8ee38a59732d304
m-sallam <muhammad.sallam@outlook.com>
2018-09-02 18:25:49 +0200

    Completed Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,37 +1,41 @@
 <template>
-  <div>
-    <el-card v-loading="loading">
-      <div slot="header" class="clearfix">
-        <span>Edit Profile</span>
-        <el-button @click="update" style="float: right; padding: 5px 0" type="text">SAVE</el-button>
-      </div>
-      <el-row>
-        <el-col :xs="24" :sm="24" :md="24" :lg="10" :xl="10" >
-          <el-row type="flex" justify="center">
-            <el-col :xs="18" :sm="18" :md="18" :lg="18" :xl="18" style="text-align:center">
+  <vs-row vs-w="12" style="padding: 60px;">
+    <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="12" vs-xs="12" >
+    </vs-col>
+    <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="8" vs-sm="12" vs-xs="12" >
+      <vs-card>
+        <div slot="header">
+          <h3> {{user.username}}'s Profile</h3>
+        </div>
+        <div>
+          <vs-row>
+            <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="5" vs-sm="12" vs-xs="12">
               <div @click="openUpload" style="cursor: pointer;">
                 <img v-if="!selectedFile && user.picture === 'none'" :src="`https://ui-avatars.com/api/?size=260&name=${user.name}&rounded=true`"  alt="profile picture">
                 <img v-if="!selectedFile && user.picture !== 'none'" :src="user.picture" alt="profile picture" width="260" height="260"  style=" object-fit: cover; border-radius:50%">
                 <img v-if="selectedFile" :src="src" alt="profile picture" width="260" height="260"  style=" object-fit: cover; border-radius:50%">           
               </div>
-              <h2>{{user.username}}</h2>
-            </el-col>
-          </el-row>
-        </el-col>
-        <el-col :xs="24" :sm="24" :md="24" :lg="14" :xl="14">
-           <el-form :model="user">
-            <el-form-item label="Name" prop="name">
-              <el-input v-model="user.name"></el-input>
-            </el-form-item>
-            <el-form-item label="Email" prop="email">
-              <el-input type="email" v-model="user.email"></el-input>
-            </el-form-item>
-          </el-form>
-        </el-col>
-      </el-row>
-    </el-card>
-    <input type="file" ref="imageUpload" style="display:none;" @change="onFileChanged"/>
-  </div>
+            </vs-col>
+            <vs-col  vs-lg="7" vs-sm="12" vs-xs="12">
+              <vs-input style="width: 100%" placeholder="Name" v-model="user.name"></vs-input>
+              <br>
+              <vs-input style="width: 100%" placeholder="Enail" v-model="user.email"></vs-input>
+              
+            </vs-col>
+          </vs-row>
+          
+        </div>
+        <div slot="footer">
+          <vs-row vs-justify="flex-end" >
+            <vs-button @click="update" vs-color="#455A64">Save Changes</vs-button>
+          </vs-row>
+        </div>
+      </vs-card>
+    </vs-col>
+    <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="12" vs-xs="12" >
+      <input type="file" ref="imageUpload" style="display:none;" @change="onFileChanged"/>
+    </vs-col>
+  </vs-row>
 </template>
 
 <script>
@@ -58,33 +62,34 @@ export default {
       this.selectedFile = event.target.files[0]
     },
     async update () {
-      this.loading = true
+      this.$vs.loading({
+        color: '#455A64',
+        scale: 0.7,
+        type: 'sound'
+      })
       if (this.selectedFile) {
         let ext = this.selectedFile.name.split('.').pop()
         let reader = new window.FileReader()
         reader.onload = async (e) => {
-          let {status} = await this.$store.dispatch('updateProfile', {src: e.target.result, ext})
-          this.loading = false
+          let { status } = await this.$store.dispatch('updateProfile', { src: e.target.result, ext })
+          this.$vs.loading.close()
           if (status) {
-            this.$message({type: 'success', message: 'Changes Saved!'})
+            this.$vs.notify({ text: 'Changes Saved!', color: 'success', position: 'top-center' })
           } else {
-            this.$message({type: 'error', message: 'An error occured, please contact support!'})
+            this.$vs.notify({ text: 'An error occured!', color: 'danger', position: 'top-center' })
           }
         }
         reader.readAsDataURL(this.selectedFile)
       } else {
-        let {status} = await this.$store.dispatch('updateProfile', {src: false, ext: false})
-        this.loading = false
+        let { status } = await this.$store.dispatch('updateProfile', { src: false, ext: false })
+        this.$vs.loading.close()
         if (status) {
-          this.$message({type: 'success', message: 'Changes Saved!'})
+          this.$vs.notify({ text: 'Changes Saved!', color: 'success', position: 'top-center' })
         } else {
-          this.$message({type: 'error', message: 'An error occured, please contact support!'})
+          this.$vs.notify({ text: 'An error occured!', color: 'danger', position: 'top-center' })
         }
       }
     }
-  },
-  mounted () {
-    this.$store.dispatch('disableGlobalLoading')
   }
 }
 </script>
